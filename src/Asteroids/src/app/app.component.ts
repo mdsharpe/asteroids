00c35dc3e1cc09c1111e102524ee2d92aba89ca0
Mark Williams <mark.williams@enable.com>
2024-08-16 17:35:14 +0100

    Log dat boi

--- src/Asteroids/src/app/app.component.ts
+++ src/Asteroids/src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GameStateService } from './game-state.service';
+import { environment } from '../environments/environment';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  providers: [GameStateService],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    providers: [GameStateService],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'Asteroids';
+export class AppComponent implements OnInit {
+    ngOnInit(): void {
+        console.log(environment);
+    }
+    title = 'Asteroids';
 }

401dd3b4657b5e7a1bf0196f7283ca5f38d170a5
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2024-08-16 15:16:47 +0100

    hackathon

--- src/Asteroids/src/app/game-state.service.ts
+++ src/Asteroids/src/app/game-state.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Bodies, Body, Composite, Engine, Runner } from 'matter-js';
+import { Bodies, Body, Composite, Engine, Events, Runner } from 'matter-js';
 
 @Injectable()
 export class GameStateService {
@@ -16,7 +16,7 @@ export class GameStateService {
         Runner.run(this.runner, this.engine);
 
         this.player = Bodies.rectangle(30, 45, 20, 10);
-        this.player.frictionAir = 0;
+        this.player.frictionAir = 0.05;
         Composite.add(this.engine.world, [this.player]);
 
         window.setInterval(() => {
@@ -26,5 +26,37 @@ export class GameStateService {
 
             Composite.add(this.engine.world, [asteroid]);
         }, 1000);
+
+        const keysDown = new Set<string>();
+        document.addEventListener('keydown', (event) => {
+            keysDown.add(event.code);
+        });
+        document.addEventListener('keyup', (event) => {
+            keysDown.delete(event.code);
+        });
+
+        Events.on(this.engine, 'beforeUpdate', (event) => {
+            console.log(keysDown);
+            if (keysDown.has('KeyW')) {
+                Body.applyForce(
+                    this.player,
+                    {
+                        x: this.player.position.x,
+                        y: this.player.position.y,
+                    },
+                    { x: 0, y: -0.0001 }
+                );
+            }
+            if (keysDown.has('KeyS')) {
+                Body.applyForce(
+                    this.player,
+                    {
+                        x: this.player.position.x,
+                        y: this.player.position.y,
+                    },
+                    { x: 0, y: 0.0001 }
+                );
+            }
+        });
     }
 }

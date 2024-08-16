5f0c4b481d3748f5344588bc9d4e7a9422b78aa3
Mark Williams <mark.williams@enable.com>
2024-08-16 13:13:33 +0100

    Add leaderboard for some dumb reason

--- src/Asteroids/src/app/app.routes.ts
+++ src/Asteroids/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { GamePageComponent } from './game-page/game-page.component';
 import { MenuComponent } from './menu/menu.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 export const routes: Routes = [
     {
@@ -11,4 +12,8 @@ export const routes: Routes = [
         path: 'play',
         component: GamePageComponent,
     },
+    {
+        path: 'leaderboard',
+        component: LeaderboardComponent
+    }
 ];

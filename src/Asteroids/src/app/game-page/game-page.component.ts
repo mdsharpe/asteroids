9573978a1041156e28136fd25a987a9977e4ede2
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2024-08-16 14:56:00 +0100

    hackathon

--- src/Asteroids/src/app/game-page/game-page.component.ts
+++ src/Asteroids/src/app/game-page/game-page.component.ts
@@ -56,24 +56,77 @@ export class GamePageComponent implements OnInit, OnDestroy {
     }
 
     private fitToScreen(): void {
-        if (this._render && this._worldContainer?.nativeElement) {
-            const container = this._worldContainer.nativeElement;
-
-            (<any>Render).setSize(
-                this._render,
-                container.clientWidth,
-                container.clientHeight
-            );
-
-            Render.lookAt(
-                this._render,
-                this._state.player,
-                {
-                    x: 200,
-                    y: 30,
-                },
-                true
-            );
+        if (!this._render || !this._worldContainer?.nativeElement) {
+            return;
         }
+
+        const container = this._worldContainer.nativeElement;
+        const render = this._render;
+        const player = this._state.player;
+
+        (<any>Render).setSize(
+            render,
+            container.clientWidth,
+            container.clientHeight
+        );
+
+        const padding = {
+            x: 200,
+            y: 30,
+        };
+
+        var bounds = {
+            min: { x: Infinity, y: Infinity },
+            max: { x: -Infinity, y: -Infinity },
+        };
+
+        let min = player.bounds.min,
+            max = player.bounds.max;
+
+        min.x += 150;
+        max.x += 150;
+
+        if (min.x < bounds.min.x) bounds.min.x = min.x;
+        if (max.x > bounds.max.x) bounds.max.x = max.x;
+        if (min.y < bounds.min.y) bounds.min.y = min.y;
+        if (max.y > bounds.max.y) bounds.max.y = max.y;
+
+        // find ratios
+        var width = bounds.max.x - bounds.min.x + 2 * padding.x,
+            height = bounds.max.y - bounds.min.y + 2 * padding.y,
+            viewHeight = render.canvas.height,
+            viewWidth = render.canvas.width,
+            outerRatio = viewWidth / viewHeight,
+            innerRatio = width / height,
+            scaleX = 1,
+            scaleY = 1;
+
+        // find scale factor
+        if (innerRatio > outerRatio) {
+            scaleY = innerRatio / outerRatio;
+        } else {
+            scaleX = outerRatio / innerRatio;
+        }
+
+        // enable bounds
+        render.options.hasBounds = true;
+
+        // position and size
+        render.bounds.min.x = bounds.min.x;
+        render.bounds.max.x = bounds.min.x + width * scaleX;
+        render.bounds.min.y = bounds.min.y;
+        render.bounds.max.y = bounds.min.y + height * scaleY;
+
+        // center
+        render.bounds.min.x += width * 0.5 - width * scaleX * 0.5;
+        render.bounds.max.x += width * 0.5 - width * scaleX * 0.5;
+        render.bounds.min.y += height * 0.5 - height * scaleY * 0.5;
+        render.bounds.max.y += height * 0.5 - height * scaleY * 0.5;
+
+        // padding
+        render.bounds.min.x -= padding.x;
+        render.bounds.max.x -= padding.x;
+        render.bounds.min.y -= padding.y;
+        render.bounds.max.y -= padding.y;
     }
 }

c817497cead774cb2fc93cec86848d97fcc7c126
Thomas Holmes <thomas.holmes@enable.com>
2024-08-16 16:30:10 +0100

    Add player moved hub method

--- src/AsteroidHub/AsteroidGameHub.cs
+++ src/AsteroidHub/AsteroidGameHub.cs
@@ -5,15 +5,21 @@ namespace AsteroidHub
 {
     public class AsteroidGameHub : Hub
     {
-        public async Task BroadcastNewAsteroid(
-            Asteroid asteroid) =>
-            await Clients.All.SendAsync(
-                "newAsteroid", 
-                asteroid.Width, 
-                asteroid.Height, 
-                asteroid.VerticalPos, 
-                asteroid.HorizontalPos, 
-                asteroid.VelocityX, 
-                asteroid.VelocityY);
+        //public async Task BroadcastNewAsteroid(
+        //    Asteroid asteroid) =>
+        //    await Clients.All.SendAsync(
+        //        "newAsteroid", 
+        //        asteroid.Width, 
+        //        asteroid.Height, 
+        //        asteroid.VerticalPos, 
+        //        asteroid.HorizontalPos, 
+        //        asteroid.VelocityX, 
+        //        asteroid.VelocityY);
+
+        public async Task BroadcastPlayer(
+            Player player) =>
+                await Clients.All.SendAsync(
+                    "playerMoved",
+                    player);
     }
 }
---
+++ src/AsteroidHub/Models/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace AsteroidHub.Models
+{
+    public class Player
+    {
+        public Guid Id { get; set; }
+        public int YPos { get; set; }
+        public int VelocityY { get; set; }
+        public int VelocityX { get; set; }
+    }
+}

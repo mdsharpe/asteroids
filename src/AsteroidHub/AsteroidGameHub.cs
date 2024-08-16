53e884958309d8d691c65244ad244f17045246f6
steven.birks <steven.birks@enable.com>
2024-08-16 16:56:53 +0100

    Broadcast player id on death

--- src/AsteroidHub/AsteroidGameHub.cs
+++ src/AsteroidHub/AsteroidGameHub.cs
@@ -5,21 +5,17 @@ namespace AsteroidHub
 {
     public class AsteroidGameHub : Hub
     {
-        //public async Task BroadcastNewAsteroid(
-        //    Asteroid asteroid) =>
-        //    await Clients.All.SendAsync(
-        //        "newAsteroid", 
-        //        asteroid.Width, 
-        //        asteroid.Height, 
-        //        asteroid.VerticalPos, 
-        //        asteroid.HorizontalPos, 
-        //        asteroid.VelocityX, 
-        //        asteroid.VelocityY);
-
         public async Task BroadcastPlayer(
             Player player) =>
                 await Clients.All.SendAsync(
                     "playerMoved",
                     player);
+        )
+
+        public async Task PlayerDead()
+            Guid playerId) => 
+                await Clients.All.SendAsync(
+                    "playerDead",
+                    playerId);
     }
 }

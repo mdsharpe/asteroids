16451dd4992e0db0eac65ad24a0080bdc9220448
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2024-08-16 12:41:17 +0100

    STUFF

--- src/Asteroids/src/app/app.routes.ts
+++ src/Asteroids/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { GamePageComponent } from './game-page/game-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'play',
+    component: GamePageComponent,
+  },
+];
